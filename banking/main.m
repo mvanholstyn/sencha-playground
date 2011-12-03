576fbb43078d390172d4e20f7436cd1573efe5e5
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-12-02 22:37:22 -0500

    Initial Commit

---
+++ banking/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  banking
+//
+//  Created by Mark Van Holstyn on 12/2/11.
+//  Copyright Mutually Human Software 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
